1f8c79ff0fafca48f02533dd7b1eec767257a3d4
Adonay Carias <Adonay Carias@HP>
2017-10-10 23:38:01 -0600

    Agregue

--- Mini-manual-Git/Form1.Designer.cs
+++ Mini-manual-Git/Form1.Designer.cs
@@ -59,9 +59,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(72, 142);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(185, 20);
+            this.label2.Size = new System.Drawing.Size(189, 20);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Sistema de Versiones git";
+            this.label2.Text = "Sistema de Versiones Git";
             // 
             // Form1
             // 
