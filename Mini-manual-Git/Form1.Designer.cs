ec2bf08af0eeaf331f5729fc44472bad18dc9ab9
Adonay Carias <Adonay Carias@HP>
2017-10-10 23:46:06 -0600

    agregando mas

--- Mini-manual-Git/Form1.Designer.cs
+++ Mini-manual-Git/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(443, 156);
+            this.button1.Location = new System.Drawing.Point(448, 171);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 27);
             this.button1.TabIndex = 1;
